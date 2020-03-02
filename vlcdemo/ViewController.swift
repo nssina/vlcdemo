3178d46b9cfec927b0142893b6c55a71c2beb5d3
nssina <nssina@icloud.com>
2020-03-02 12:30:00 -0500

    test 10

---
+++ vlcdemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vlcdemo
+//
+//  Created by Sina Rabiei on 3/2/20.
+//  Copyright © 2020 Sina Rabiei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
